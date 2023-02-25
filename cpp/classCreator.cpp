a517c3067c07d0b6d76975638dda736da2e753ee
loku74 <loicbn7@gmail.com>
2023-02-25 17:58:18 +0100

    classCreator edit: -c flag now creates a colors.h header and include it in the .hpp file. If -s and -c flag are presents, debug messages are printed in colors

--- cpp/classCreator.cpp
+++ cpp/classCreator.cpp
@@ -1,50 +1,104 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <filesystem>
 
-static void	create_src_file( std::ofstream & src_file, std::string & class_name, bool & status)
+static void	create_src_file( std::ofstream & src_file, std::string & class_name, bool & status, bool & colors )
 {
 	src_file << "#include \"" << class_name + ".hpp" << "\"\n\n";
 	src_file << class_name << "::" << class_name << "( void )\n{\n";
 	if (status)
-		src_file << "\tstd::cout << \"(" << class_name << ") -> Default constructor called\" << std::endl;";
+	{
+		src_file << "\tstd::cout << ";
+		if (colors)
+			src_file << "GRAY << ";
+		src_file << "\"(" << class_name << ")";
+		if (colors)
+			src_file << "\" << GREEN << \"";
+		src_file << " -> Default constructor called\" << ";
+		if (colors)
+			src_file << "NC << ";
+		src_file << "std::endl;";
+	}
 	src_file << "\n}\n\n\n";
 	src_file << class_name << "::" << class_name << "( " << class_name << " const & toCopy )\n{\n";
 	if (status)
-		src_file << "\t" << "std::cout << \"(" << class_name << ") -> Copy constructor called\" << std::endl;\n\n";
+	{
+		src_file << "\t" << "std::cout << ";
+		if (colors)
+			src_file << "GRAY << ";
+		src_file << "\"(" << class_name << ") -> Copy constructor called\" << ";
+		if (colors)
+			src_file << "NC << ";
+		src_file << "std::endl;\n\n";
+	}
 	src_file << "\t*this = toCopy;\n}\n\n\n";
 	src_file << class_name << "::~" << class_name << "( void )\n{\n";
 	if (status)
-		src_file << "\tstd::cout << \"(" << class_name << ") -> Destructor called\" << std::endl;";
+	{
+		src_file << "\tstd::cout << ";
+		if (colors)
+			src_file << "GRAY << ";
+		src_file << "\"(" << class_name << ")";
+		if (colors)
+			src_file << "\" << RED << \"";
+		src_file << " -> Default destructor called\" << ";
+		if (colors)
+			src_file << "NC << ";
+		src_file << "std::endl;";
+	}
 	src_file << "\n}\n\n\n";
 	src_file << class_name << " &\t" << class_name << "::operator=( " << class_name << " const & toAssign )\n{\n";
 	if (status)
-		src_file << "\t" << "std::cout << \"(" << class_name << ") -> Assignment operator called\" << std::endl;\n\n";
+	{
+		src_file << "\t" << "std::cout << ";
+		if (colors)
+			src_file << "GRAY << ";
+		src_file << "\"(" << class_name << ") -> Assignment operator called\" << ";
+		if (colors)
+			src_file << "NC << ";
+		src_file << "std::endl;\n\n";
+	}
 	src_file << "\treturn (*this);\n}" << std::endl;
 }
 
 
 static void	create_header_file( std::ofstream & header_file, std::string & class_name, bool& setDefineColors )
 {
-	header_file << "#pragma once\n\n" << "#include <iostream>\n\n";
+	header_file << "#pragma once\n\n" << "#include <iostream>\n";
 	if (setDefineColors)
-	{
-		header_file << "#define RED \"\\033[1;31m\"\n";
-		header_file << "#define GREEN \"\\033[1;32m\"\n";
-		header_file << "#define YELLOW \"\\033[1;33m\"\n";
-		header_file << "#define ORANGE \"\\033[38;5;208m\"\n";
-		header_file << "#define PURPLE \"\\033[1;35m\"\n";
-		header_file << "#define BLUE \"\\033[1;36m\"\n";
-		header_file << "#define GRAY \"\\033[1;90m\"\n";
-		header_file << "#define NC \"\\033[0m\"\n\n";
-	}
-	header_file << "class " << class_name << "\n{\n\n";
+		header_file << "#include \"colors.h\"\n";
+	header_file << "\nclass " << class_name << "\n{\n\n";
 	header_file << "public:\n\n";
 	header_file << "\t" << class_name << "( void );\n";
 	header_file << "\t" << class_name << "( " << class_name << " const & toCopy );\n";
 	header_file << "\t~" << class_name << "( void );\n\n";
 	header_file << "\t" << class_name << " &\toperator=( " << class_name << " const & toAssign );\n\n";
 	header_file << "private:\n\n};" << std::endl;
+}
+
+
+static void	create_color_header( void )
+{
+	std::ofstream	color_header;
+
+	color_header.open("colors.h", std::ios::out);
+	if (!color_header.is_open())
+	{
+		std::cerr << "Couldn't create the \"colors.h\" header." << std::endl;
+		return ;
+	}
+	color_header << "#ifndef COLORS_H\n";
+	color_header << "# define COLORS_H\n\n";
+	color_header << "# define RED \"\\033[1;31m\"\n";
+	color_header << "# define GREEN \"\\033[1;32m\"\n";
+	color_header << "# define YELLOW \"\\033[1;33m\"\n";
+	color_header << "# define ORANGE \"\\033[38;5;208m\"\n";
+	color_header << "# define PURPLE \"\\033[1;35m\"\n";
+	color_header << "# define BLUE \"\\033[1;36m\"\n";
+	color_header << "# define GRAY \"\\033[1;90m\"\n";
+	color_header << "# define NC \"\\033[0m\"\n";
+	color_header << "\n#endif" << std::endl;
 }
 
 
@@ -103,12 +157,15 @@ int	main( int argc, char **argv )
 		return (2);
 	}
 
+	if (setDefineColors && !std::filesystem::exists("colors.h"))
+		create_color_header();
+
 	for (int i = 0; i < class_names.size(); i++)
 	{
 		if (!create_files(class_names[i], class_header, class_src))
 		{
 			create_header_file(class_header, class_names[i], setDefineColors);
-			create_src_file(class_src, class_names[i], debugMessage);
+			create_src_file(class_src, class_names[i], debugMessage, setDefineColors);
 
 			class_header.close();
 			class_src.close();
